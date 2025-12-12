db50005615b84a325e0c8405435c5fa13e1b45bc
iv4n89 <ivan.betanzos89@gmail.com>
2025-12-12 04:03:19 +0100

    fix: main

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,12 @@ async function bootstrap() {
   app.useGlobalPipes(
     new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }),
   );
-  app.enableCors();
-  await app.listen(process.env.PORT ?? 3000);
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    allowedHeaders: 'Content-Type, Accept',
+  });
+  const port = process.env.PORT || 3000;
+  await app.listen(port, '0.0.0.0');
 }
 bootstrap();
